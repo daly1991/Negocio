7001ebc000c9aa019a19b43a3de1bc40ae988c85
elrafa1981 <rafaelcarrasco88@gmail.com>
2022-06-11 19:37:56 -0300

    Agregar archivos de proyecto.

---
+++ Negocio/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class Class1
+    {
+        //comentario creado en la linea 11, programacion web
+
+    }
+
+}
